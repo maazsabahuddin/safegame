cafa5b63c25721f8265f21c4808cf4d28c8dbc12
Maaz Sabah Uddin <maazsabahuddin@gmail.com>
2024-11-14 14:52:10 -0500

    update documenatation of the api views and update serialization errors with respect to the drf

--- library/views.py
+++ library/views.py
@@ -15,6 +15,10 @@ from django.db import transaction
 @api_view(['GET'])
 @decorators.logging
 def books_with_authors(request):
+    """
+    This API will return the all the books with the author name and the tags associated with the book.
+    :param request:
+    """
     # Retrieve all books with author and tags (optimized with select_related and prefetch_related)
     books = Book.objects.select_related('author').prefetch_related('tags').all()
     serializer = BookSerializer(books, many=True)
@@ -28,6 +32,10 @@ def books_with_authors(request):
 @api_view(['GET'])
 @decorators.logging
 def get_authors(request):
+    """
+    This API will return the all the Authors in the database.
+    :param request:
+    """
     authors = Author.objects.all()
     serializer = AuthorSerializer(authors, many=True)
     response = response_utils.get_response_object(response_code=status.HTTP_200_OK,
@@ -40,12 +48,15 @@ def get_authors(request):
 @decorators.logging
 @decorators.validator(['title', 'author', 'tags'])
 def create_book(request):
+    """
+    This API will return create the book and associate it with the author name and the tags provided.
+    :param request:
+    """
     serializer = BookSerializer(data=request.data)
 
     if not serializer.is_valid():
         response = response_utils.get_response_object(response_code=status.HTTP_400_BAD_REQUEST,
-                                                      response_data=serializer.data,
-                                                      response_message="Serializer Not Valid")
+                                                      response_message=serializer.errors)
         return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
     with transaction.atomic():
